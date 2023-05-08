06f5bdc35de1f70d80df0817699098d88e76d75a
chris_1698 <christopher_1698@hotmail.com>
2023-05-08 21:07:05 +0200

    feat: mock for register page

---
+++ src/pages/authentication/UserRegister.tsx
@@ -0,0 +1,113 @@
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { useNavigate } from 'react-router-dom';
+import { Button, FormControl, Typography } from '@material-ui/core';
+import { TextField } from '@mui/material';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { useClerk, useSession } from '@clerk/clerk-react';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const UserRegister = () => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [emailError, setEmailError] = useState();
+    const [passwordError, setPasswordError] = useState();
+    const { t } = useTranslation();
+
+    const navigate = useNavigate();
+
+    const onClickButton = () => {
+        //TODO
+    };
+
+    return (
+        <>
+            <Grid
+                container
+                spacing={0}
+                direction='column'
+                alignItems='center'
+                justifyContent='center'
+            >
+                <Box
+                sx={{
+                    width: '70%',
+                    height: 800,
+                    backgroundColor: 'primary.main',
+                }}
+                borderRadius='3%'
+                >
+                <Grid pt={20} container justifyContent='center' alignItems='center'>
+                    <Typography component='h3' variant='h3'>
+                    {t('session.signUp')}
+                    </Typography>
+                </Grid>
+
+                <br />
+                <Grid className='dataInput' container justifyContent='center'>
+                    <TextField 
+                        sx={{ width:600 }}
+                        // value={username}
+                        label={t('session.username')}
+
+                    />
+                </Grid>
+                <br />
+                <Grid className='dataInput' container justifyContent='center'>
+                    <TextField
+                    sx={{ width: 600 }}
+                    value={email}
+                    label={t('session.email')}
+                    // onChange={(ev) => setEmail(ev.target.value)}
+                    className='inputBox'
+                    />
+                    <label className='errorLabel'>{emailError}</label>
+                </Grid>
+
+                <br />
+                <Grid className='dataInput' container justifyContent='center'>
+                <TextField
+                    sx={{ width: 600 }}
+                    type='password'
+                    value={password}
+                    label={t('session.password')}
+                    // onChange={(ev) => setPassword(ev.target.value)}
+                />    
+                </Grid>
+                <br />
+                <Grid className='dataInput' container justifyContent='center'>
+                    <TextField 
+                        sx={{ width: 600 }}
+                        type='password'
+                        // value={passwordAgain}
+                        label={t('session.repeatPassword')}
+                    />
+                </Grid>
+                <br />
+                <Grid className='dataInput' container justifyContent='center'>
+                    <TextField 
+                        sx={{ width: 600 }}
+                        type='date'
+                        //value={}
+                    />
+                </Grid>
+                <br />
+                <Grid container justifyContent='center' alignContent='center'>
+                    <div className='buttonContainer'>
+                    <Button
+                        className='inputButton'
+                        onClick={onClickButton}
+                        variant='contained'
+                    >
+                        {t('session.login')}
+                    </Button>
+                    </div>
+                </Grid>
+                </Box>
+            </Grid>
+        </>
+    );
+};
+
+export default UserRegister;
